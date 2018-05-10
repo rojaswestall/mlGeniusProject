8de5333f9f7b353dc9218a5a6e23c0b784b788ab
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-05-09 19:29:33 -0500

    return an artist id given a string

--- genius_api.py
+++ genius_api.py
@@ -4,9 +4,9 @@ import requests
 import urllib2
 import json
 
-AUTH_TOKEN = ""
+AUTH_TOKEN = "4OJhKq4UxyXPDNw9BM9BxvLwHtdGxcmwTtPzv_toigTps1vaVvbYow8cg-v0A5z4"
 
-
+# Search genius, given a string. Search can return anything
 def genius_search(term):
     _URL_API = "https://api.genius.com/"
     _URL_SEARCH = "search?q="
@@ -20,7 +20,18 @@ def genius_search(term):
     json_obj = json.loads(raw)
     return json_obj
 
+# Return the id of an artist given a search term
+def genius_search_artist(artist):
+    songres = genius_search(artist)
+    hits = songres['response']['hits']
 
+    if len(songres['response']['hits']) == 0:
+        print 'There were no artists that matched the search'
+        return -1
+    else:
+        return songres['response']['hits'][0]['result']['primary_artist']['id']
+
+# Get songs for an artist given an artists ID
 def genius_artist_songs(artist_id):
     _URL_API = "https://api.genius.com/"
     _URL_ARTIST = "artists/{}/songs?sort=popularity&per_page=50".format(artist_id)
@@ -36,7 +47,10 @@ def genius_artist_songs(artist_id):
 
 
 if __name__ == '__main__':
-    # res = genius_search('Andy Shauf')
+    res = genius_search_artist('Post Malone')
+    print json.dumps(res, indent=4)
+    # # print res
+    # print '\n'
     # print res.viewkeys()
     # print "\n"
     # print [key for key in res['response']['hits'][0]['result']]
