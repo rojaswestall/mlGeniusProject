571c0316212f896ff800a1759958d76d9fd82937
M. Smith <animaniac21@gmail.com>
2018-05-09 19:54:55 -0500

    artist_name->id->songs working

--- genius_api.py
+++ genius_api.py
@@ -6,8 +6,9 @@ import json
 
 AUTH_TOKEN = "4OJhKq4UxyXPDNw9BM9BxvLwHtdGxcmwTtPzv_toigTps1vaVvbYow8cg-v0A5z4"
 
-# Search genius, given a string. Search can return anything
+
 def genius_search(term):
+    """Search genius, given a string. Search can return anything"""
     _URL_API = "https://api.genius.com/"
     _URL_SEARCH = "search?q="
     querystring = _URL_API + _URL_SEARCH + urllib2.quote(term)
@@ -20,21 +21,22 @@ def genius_search(term):
     json_obj = json.loads(raw)
     return json_obj
 
-# Return the id of an artist given a search term
-def genius_search_artist(artist):
-    songres = genius_search(artist)
-    hits = songres['response']['hits']
 
+def genius_search_artist(artist):
+    """Return the id of an artist given a search term"""
+    songres = genius_search(artist)
+    # hits = songres['response']['hits']
     if len(songres['response']['hits']) == 0:
         print 'There were no artists that matched the search'
         return -1
     else:
         return songres['response']['hits'][0]['result']['primary_artist']['id']
 
-# Get songs for an artist given an artists ID
-def genius_artist_songs(artist_id):
+
+def genius_artist_songs(artist_id, num):
+    """ Get songs for an artist given an artists ID"""
     _URL_API = "https://api.genius.com/"
-    _URL_ARTIST = "artists/{}/songs?sort=popularity&per_page=50".format(artist_id)
+    _URL_ARTIST = "artists/{}/songs?sort=popularity&per_page={}".format(artist_id, str(num))
     querystring = _URL_API + _URL_ARTIST
     request = urllib2.Request(querystring)
     request.add_header("Authorization", "Bearer " + AUTH_TOKEN)
@@ -47,14 +49,16 @@ def genius_artist_songs(artist_id):
 
 
 if __name__ == '__main__':
-    res = genius_search_artist('Post Malone')
-    print json.dumps(res, indent=4)
-    # # print res
-    # print '\n'
-    # print res.viewkeys()
-    # print "\n"
-    # print [key for key in res['response']['hits'][0]['result']]
 
-    # res = genius_artist_songs('16775')
-    # for i in range(len(res['response']['songs'])):
-    #     print res['response']['songs'][i]['full_title']
+    artists = ["2Pac", "Eminem", "Ice Cube", "Outkast", "Nas", "DMX", 
+               "The Game", "T.I.", "Kanye West", "Kendrick Lamar"]
+    for at in artists:
+        at_id = genius_search_artist(at)
+
+        if at_id == -1:
+            continue
+        print "\n"
+        print at
+        at_songs = genius_artist_songs(at_id, 5)
+        for i in range(len(at_songs['response']['songs'])):
+            print at_songs['response']['songs'][i]['full_title']
