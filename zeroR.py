0fa4ee396e9889de4584272529eb54504f6efce5
M. Smith <animaniac21@gmail.com>
2018-05-20 16:52:14 -0500

    zeroR for total votes

---
+++ zeroR.py
@@ -0,0 +1,22 @@
+from sklearn.metrics import zero_one_loss
+import pandas as pd
+
+
+def zeroR(file):
+    data = pd.read_csv(file)
+
+    print data['Total_Votes'].values
+    print len(data['Total_Votes'].values)
+    num_values = len(data['Total_Votes'].values)
+
+    mean = sum(data['Total_Votes'].values) / num_values
+
+    y_pred = [mean] * num_values
+    y_true = data['Total_Votes'].values
+
+    print "ZeroR Accuracy%: {}".format(1 - zero_one_loss(y_true, y_pred))
+    print "Correct: {}/{}".format(num_values - zero_one_loss(y_true, y_pred, normalize=False), num_values)
+
+
+if __name__ == '__main__':
+    zeroR("data.csv")
