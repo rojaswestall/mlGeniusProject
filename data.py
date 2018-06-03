e550fe51e22bbb65972c637cbedcfe1faeed171a
chits22 <chits22@gmail.com>
2018-06-03 17:29:33 -0500

    datasets

--- data.py
+++ data.py
@@ -19,3 +19,15 @@ test = df[~msk]
 train.to_csv('./train.csv')
 test.to_csv('./test.csv')
 
+
+f = './test.csv'
+df = pandas.read_csv(f)
+median = df['Total_Votes'].median()
+
+df['Class'] = df['Total_Votes']
+
+for x in df['Total_Votes']:
+	if x>=median:
+		df['Class'] = "High"
+	else:
+		df['Class'] = 'Low' 
